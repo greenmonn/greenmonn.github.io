3ec4b96124e6f581b5725bdb6afd0479960c47fb
greenmon <greenmon@kaist.ac.kr>
2019-01-05 23:38:26 +0900

    Add script to migrate image file on post
    Add script to migrate image file on post
    
    Rename convert_post to format_post

--- format_post.py
+++ format_post.py
@@ -5,7 +5,7 @@ import re
 from datetime import time, date
 
 posts_path = '_posts'
-image_path = os.path.join('assets', 'img')
+image_path = os.path.join('assets', 'images')
 
 new_posts = []
 
@@ -24,27 +24,38 @@ for new_post in new_posts:
 
     print("new post found: ", post_filename)
 
+     # move images to assets/img
+    try:
+        post_img_path = shutil.move(os.path.join("_posts", post_filename.replace('.md', '')), image_path)
+        print("[*] image files moved to", post_img_path)
+    except FileNotFoundError:
+        print("[**] No image directory found: skip moving image")
+
     # read whole post file
     with open(post_path, 'r') as f:
         post_file = f.read()
 
+    # except code block
+    p = re.compile("`(.*)`", re.DOTALL)
+    post_file_without_codeblock = p.sub("", post_file)
+
     # extract post title (assume H1 tag on the top of the file is containing title)
-    in_post_titles = re.findall("^# .*", post_file)
-    post_file = re.sub("^# .*\n", "", post_file)
+    in_post_titles = re.findall("^# .*", post_file_without_codeblock)
 
     if len(in_post_titles) is 0:
         post_title = post_filename.replace('-', ' ').replace('.md', '')
 
     else:
+        post_file = post_file.replace(in_post_titles[0] + '\n', "")
+        
         post_title = re.sub("# ", "", in_post_titles[0]).strip()
     
     print("[*] post title extracted: ", post_title)
 
     # extract post tag (except # in source code block wrapped by ``` or `
-    re_tag = re.compile("`(.*)`", re.DOTALL)
-    post_file_without_codeblock = re_tag.sub("", post_file)
-    print(post_file_without_codeblock)
-    post_tags = re.findall("#([^#\s]+)", post_file_without_codeblock)
+    re_code = re.compile("`(.*)`", re.DOTALL)
+    post_file_without_codeblock = re_code.sub("", post_file)
+    post_tags = re.findall("#([\w+-]+)", post_file_without_codeblock)
 
     for tag in post_tags:
         post_file = post_file.replace("#" + tag, "")
@@ -52,6 +63,19 @@ for new_post in new_posts:
     post_tags.append("TIL")
 
     print("[*] post tags extracted: ", post_tags)
+
+    # find all image tags
+    img_tags = re.findall('!\[\]\(.*\)', post_file)
+
+    # change image path in image tag
+    for idx, img_tag in enumerate(img_tags):
+        new_path = os.path.join(image_path, img_tag[4:-1])
+        new_tag = '![](/' + new_path + ')'
+        post_file = post_file.replace(img_tag, new_tag)
+        # use first image as thumbnail
+        if idx == 0:
+            post_thumbnail = new_path
+    print("[*] image path adjusted")
 
     # generate jekyll "front matter"
     front_matter = '---\n' \
